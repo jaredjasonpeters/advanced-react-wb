813a4d5544085ba5dc8ec582a91888d7d8b0e583
Jared Peters <jjpeters@dlfna.com>
2018-10-18 11:36:10 -0700

    added page, header, nav, meta components

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -3,9 +3,7 @@ import Link from 'next/link';
 const Home = (props) => (
     <div>
         <p>Home</p>
-        <Link href="/sell">
-            <a>Sell</a>
-        </Link>
+        
     </div>
 )
 
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -4,9 +4,7 @@ import Link from 'next/link'
 const Sell = (props) => (
     <div>
         <p>Sell</p>
-        <Link href="/">
-            <a>Home!</a>
-        </Link>
+       
     </div>
 )
 
