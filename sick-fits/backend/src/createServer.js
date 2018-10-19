d5943d40f075126606a54cebe1c07353b581e83a
Jared Peters <jjpeters@dlfna.com>
2018-10-18 23:44:49 -0700

    added graphql server / prisma

---
+++ sick-fits/backend/src/createServer.js
@@ -0,0 +1,25 @@
+const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('../src/generated/prisma/prisma-schema')
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+
+function createServer() {
+  return new GraphQLServer({
+    typeDefs: 'src/schema.graphql',
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => {
+      return {
+        ...req,
+        db: prisma
+      }
+    },
+  });
+}
+
+module.exports = createServer
