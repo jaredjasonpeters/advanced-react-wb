5a58e714b4af63db45cfe025b73d8a6bc67f8bef
Jared J. Peters <jjpeters@dlfna.com>
2018-10-30 07:40:38 -0700

    finshed 28

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -52,7 +52,36 @@ const mutations = {
     //we return user to the Browser
     return user;
 
-  }
-};
+  },
+  async signin(parent, {email, password}, ctx, info) {
+    //1. check if there is a user
+    const user = await ctx.db.query.user({ where : {email}})
+    if(!user) {
+      throw new Error(`No such user for email ${email}`);
+    }
+    //2. check if password matches
+    const valid = await bcrypt.compare(password, user.password)
+    if(!valid){
+      throw new Error('Invalid Password');
+    }
+    //3. generate JWT
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    //4. Set the cookie with the token
+    setCookieWithToken(ctx, token)
+    //5. return the user
+    return user
+   },
+   signout(parent, args, ctx, info){
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye' }
+   }
+ };
 
 module.exports = mutations;
+
+function setCookieWithToken (ctx, token) {
+  ctx.response.cookie('token', token, {
+    httpOnly: true, 
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+  })
+};
