b98f68d28017d9c48604798f8b2381034e588b39
Jared J. Peters <jjpeters@dlfna.com>
2018-11-01 12:57:08 -0700

    finished 36 backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -4,6 +4,7 @@ const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const { transport, makeANiceEmail } = require('../mail');
 const { setCookieWithToken } = require('../utils')
+const { hasPermission } = require('../utils')
 
 const mutations = {
   
@@ -170,6 +171,33 @@ const mutations = {
      setCookieWithToken(ctx, token);
      //8. return the new user
     return updatedUser;
+   },
+
+
+   async updatePermissions(parent, args, ctx, info) {
+     // 1. Check if they are logged in
+      if (!ctx.request.userId) {
+        throw new Error('Must be logged in!')
+      }
+     // 2. Query the current user
+     const currentUser = await ctx.db.query.user({
+       where: {
+         id: ctx.request.userId,
+       },
+     }, info)
+     // 3. Check if they have permissions to do this
+     hasPermission(currentUser, ['ADMIN', 'PERMISSIONUPDATE'])
+     // 4. Updtate the permissions
+     return ctx.db.mutation.updateUser({
+       data: {
+         permissions: {
+           set: args.permissions,
+         }
+       },
+       where: { 
+         id: args.userId
+       }
+     }, info);
    }
   };
 
