98af0218f4116020c739eb9785564e4e3450233b
Jared Peters <jjpeters@dlfna.com>
2018-11-02 21:26:04 -0700

    finished 39

--- sick-fits/frontend/components/Cart.js
+++ sick-fits/frontend/components/Cart.js
@@ -1,25 +1,55 @@
 import React from "react";
+import { Query, Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import { consolidateStreamedStyles } from "styled-components";
 import SickButton from "./styles/SickButton";
 import CartStyle from "./styles/CartStyles";
 import Supreme from "./styles/Supreme";
 import CloseButton from "./styles/CloseButton";
 
+const LOCAL_STATE_QUERY = gql`
+  query {
+    cartOpen @client
+  }
+`
+const TOGGLE_CART_MUTATION = gql`
+  mutation {
+    toggleCart @client
+  }
+`
+
 const Cart = () => {
   return (
-    <CartStyle>
-      <header>
-        <CloseButton title="close">&times;</CloseButton>
-        <Supreme>Your Cart</Supreme>
-        <p> You Have __ Items in your cart.</p>
-      </header>
-
-      <footer>
-        <p>$10.10</p>
-        <SickButton> Checkout </SickButton>
-      </footer>
-    </CartStyle>
+    <Mutation mutation={TOGGLE_CART_MUTATION}>
+    {(toggleCart) => {
+      return (
+        <Query query={LOCAL_STATE_QUERY} >
+        {({data, loading, error}) => {
+          
+          return (
+            <CartStyle open={data.cartOpen}>
+            <header>
+              <CloseButton title="close" onClick={toggleCart}>&times;</CloseButton>
+              <Supreme>Your Cart</Supreme>
+              <p> You Have __ Items in your cart.</p>
+            </header>
+    
+            <footer>
+              <p>$10.10</p>
+              <SickButton> Checkout </SickButton>
+            </footer>
+          </CartStyle>
+          )
+        }}
+        </Query>
+      )
+    }}
+    </Mutation>
   );
 };
 
 export default Cart;
+export { 
+  LOCAL_STATE_QUERY,
+  TOGGLE_CART_MUTATION,
+}
--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,7 +1,9 @@
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
+import {TOGGLE_CART_MUTATION} from './Cart';
 
 const Nav = (props) => (
     <User>
@@ -30,6 +32,13 @@ const Nav = (props) => (
                             <a>Account</a>
                         </Link>
                         <Signout />
+                        <Mutation mutation={TOGGLE_CART_MUTATION}>
+                            {toggleCart => {
+                                return (
+                                    <button onClick={toggleCart}>My Cart</button>
+                                )
+                            }}
+                        </Mutation>
                     </>
                 )
                 }
