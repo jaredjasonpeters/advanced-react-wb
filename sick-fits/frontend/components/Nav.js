cb9c40af3022c70e25d12c4349817e07f755085a
Jared J. Peters <jjpeters@dlfna.com>
2018-11-06 14:21:08 -0800

    finished 44 fe

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,52 +1,59 @@
-import Link from 'next/link';
-import { Mutation } from 'react-apollo';
-import NavStyles from './styles/NavStyles';
-import User from './User';
-import Signout from './Signout';
-import {TOGGLE_CART_MUTATION} from './Cart';
+import Link from "next/link";
+import { Mutation } from "react-apollo";
+import NavStyles from "./styles/NavStyles";
+import User from "./User";
+import Signout from "./Signout";
+import { TOGGLE_CART_MUTATION } from "./Cart";
+import CartCount from "./CartCount";
 
-const Nav = (props) => (
-    <User>
-        {({data: { me }}) => (
-            <NavStyles>
-                {me && <p>{me.name}</p>}
-                <Link href="/items">
-                    <a>Shop</a>
-                </Link>
-                {!me &&
-                     <Link href="/signup">
-                        <a>Sign In</a>
-                    </Link>
-                }
-                {me && (
-                    <>
-                        <Link href="/sell">
-                            <a>Sell</a>
-                        </Link>
-                        
-                        <Link href="/orders">
-                            <a>Orders</a>
-                        </Link>
-                        
-                        <Link href="/me">
-                            <a>Account</a>
-                        </Link>
-                        <Signout />
-                        <Mutation mutation={TOGGLE_CART_MUTATION}>
-                            {toggleCart => {
-                                return (
-                                    <button onClick={toggleCart}>My Cart</button>
-                                )
-                            }}
-                        </Mutation>
-                    </>
-                )
-                }
-        
-            </NavStyles>
-         )   
-        }
-    </User>
-)
+const Nav = props => (
+  <User>
+    {({ data: { me } }) => {
+      return (
+        <NavStyles>
+          {me && <p>{me.name}</p>}
+          <Link href="/items">
+            <a>Shop</a>
+          </Link>
+          {!me && (
+            <Link href="/signup">
+              <a>Sign In</a>
+            </Link>
+          )}
+          {me && (
+            <>
+              <Link href="/sell">
+                <a>Sell</a>
+              </Link>
 
-export default Nav
+              <Link href="/orders">
+                <a>Orders</a>
+              </Link>
+
+              <Link href="/me">
+                <a>Account</a>
+              </Link>
+              <Signout />
+              <Mutation mutation={TOGGLE_CART_MUTATION}>
+                {toggleCart => {
+                  return (
+                    <button onClick={toggleCart}>
+                      My Cart
+                      <CartCount
+                        count={me.cart.reduce((tally, cartItem) => {
+                          return tally + cartItem.quantity;
+                        }, 0)}
+                      />
+                    </button>
+                  );
+                }}
+              </Mutation>
+            </>
+          )}
+        </NavStyles>
+      );
+    }}
+  </User>
+);
+
+export default Nav;
