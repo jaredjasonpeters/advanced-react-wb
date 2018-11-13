a9427e9c55bc1a3e21671498cbd37ad5d01e6359
Jared J. Peters <jjpeters@dlfna.com>
2018-11-13 11:37:13 -0800

    finished 52 be

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,27 +1,53 @@
-const { forwardTo } = require('prisma-binding')
-const { hasPermission } = require('../utils')
+const { forwardTo } = require("prisma-binding");
+const { hasPermission } = require("../utils");
 
 const Query = {
-  items: forwardTo('db'),
-  item: forwardTo('db'),
-  itemsConnection: forwardTo('db'),
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
   async users(parent, args, ctx, info) {
     // 1. Check if they are logged in
     if (!ctx.request.userId) {
-      throw new Error('You must be logged in!')
+      throw new Error("You must be logged in!");
     }
     // 2. Check if the user has permissiion to query all the users
-    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
     // 3. If they do, query all the users!
-    const users = await ctx.db.query.users({}, info)
-    return users
+    const users = await ctx.db.query.users({}, info);
+    return users;
+  },
+  async order(parent, args, ctx, info) {
+    //1. Make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error("You aren't logged in");
+    }
+    //2. Query the current order
+    const order = await ctx.db.query.order(
+      {
+        where: { id: args.id }
+      },
+      info
+    );
+    //3. Check if they have the permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId;
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
+      "ADMIN"
+    );
+    if (!ownsOrder || !hasPermission) {
+      throw new Error("You can't see this budd!");
+    }
+    //4. reurn the order
+    return order;
   },
   me(parent, args, ctx, info) {
     //check if there is a current userId
-    if(!ctx.request.userId) return null
-    return ctx.db.query.user({
-      where: { id: ctx.request.userId },
-    }, info);
+    if (!ctx.request.userId) return null;
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
   }
 };
 
